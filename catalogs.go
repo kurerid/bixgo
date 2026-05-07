9af862583ebfbd0b301a8ce12d12fe18f13cfb9b
vasnin10a22syu <kurerid@yandex.ru>
2026-05-07 16:39:46 +0500

    main Adds Catalog struct with custom unmarshal

--- catalogs.go
+++ catalogs.go
@@ -3,13 +3,69 @@ package bixgo
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 )
 
+type Catalog struct {
+	Id              int    `json:"id"`
+	IblockId        int    `json:"iblockId"`
+	IblockTypeId    string `json:"iblockTypeId"`
+	Lid             string `json:"lid"`
+	Name            string `json:"name"`
+	ProductIblockId *int   `json:"productIblockId"`
+	SkuPropertyId   *int   `json:"skuPropertyId"`
+	Subscription    string `json:"subscription"`
+	VatId           *int   `json:"vatId"`
+	CustomFields    map[string]any
+}
+
 type CatalogsResponse struct {
-	Catalogs []any `json:"catalogs"`
+	Catalogs []Catalog `json:"catalogs"`
 }
 
 const catalogsListMethod = "catalog.catalog.list"
+
+var knownFields = map[string]struct{}{
+	"id":              {},
+	"iblockId":        {},
+	"iblockTypeId":    {},
+	"lid":             {},
+	"name":            {},
+	"productIblockId": {},
+	"skuPropertyId":   {},
+	"subscription":    {},
+	"vatId":           {},
+}
+
+func (c *Catalog) UnmarshalJSON(data []byte) error {
+	type Alias Catalog
+	if err := json.Unmarshal(data, (*Alias)(c)); err != nil {
+		return err
+	}
+
+	var raw map[string]json.RawMessage
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+
+	customFieldsCount := len(raw) - len(knownFields)
+	if customFieldsCount <= 0 {
+		return nil
+	}
+
+	c.CustomFields = make(map[string]any, customFieldsCount)
+	for key, val := range raw {
+		if _, found := knownFields[key]; !found {
+			var v any
+			if err := json.Unmarshal(val, &v); err != nil {
+				return fmt.Errorf("unmarshal custom field %s: %w", key, err)
+			}
+			c.CustomFields[key] = v
+		}
+	}
+
+	return nil
+}
 
 func (c *Client) GetCatalogs(
 	ctx context.Context,
