681f4dad665e36230d9fde4622f7862b757721c8
vasnin10a22syu <kurerid@yandex.ru>
2026-05-12 16:29:21 +0500

    main Adds bitrix data structs

--- common.go
+++ common.go
@@ -18,3 +18,101 @@ type BitrixProductPicture struct {
 	Url        string `json:"url"`
 	UrlMachine string `json:"urlMachine"`
 }
+
+type BitrixChecklistItem struct {
+	Id               string                    `json:"id"`
+	TaskId           *string                   `json:"taskId"`
+	CreatedBy        *string                   `json:"createdBy"`
+	ParentId         *string                   `json:"parentId"`
+	Title            string                    `json:"title"`
+	SortIndex        string                    `json:"sortIndex"`
+	IsComplete       BitrixBoolean             `json:"isComplete"`
+	IsImportant      BitrixBoolean             `json:"isImportant"`
+	ToggledBy        *string                   `json:"toggledBy"`
+	ToggledDate      *string                   `json:"toggledDate"`
+	UfChecklistFiles bool                      `json:"ufChecklistFiles"`
+	Members          []BitrixUserDescription   `json:"members"`
+	Attachments      []any                     `json:"attachments"`
+	EntityId         *string                   `json:"entityId"`
+	Action           BitrixChecklistItemAction `json:"action"`
+}
+
+type BitrixChecklistItemAction struct {
+	Modify        bool `json:"modify"`
+	Remove        bool `json:"remove"`
+	Toggle        bool `json:"toggle"`
+	Add           bool `json:"add"`
+	AddAccomplice bool `json:"addAccomplice"`
+}
+
+type BitrixGroup struct {
+	Id             string         `json:"id"`
+	Name           string         `json:"name"`
+	Opened         bool           `json:"opened"`
+	MembersCount   int            `json:"membersCount"`
+	Image          *string        `json:"image"`
+	AdditionalData map[string]any `json:"additionalData"`
+}
+
+type BitrixUserDescription struct {
+	Id           string  `json:"id"`
+	Name         string  `json:"name"`
+	Link         string  `json:"link"`
+	Icon         *string `json:"icon"`
+	WorkPosition *string `json:"workPosition"`
+}
+
+type BitrixAction struct {
+	Accept             bool `json:"accept"`
+	Decline            bool `json:"decline"`
+	Complete           bool `json:"complete"`
+	Approve            bool `json:"approve"`
+	Disapprove         bool `json:"disapprove"`
+	Start              bool `json:"start"`
+	Pause              bool `json:"pause"`
+	Delegate           bool `json:"delegate"`
+	Remove             bool `json:"remove"`
+	Edit               bool `json:"edit"`
+	Defer              bool `json:"defer"`
+	Renew              bool `json:"renew"`
+	Create             bool `json:"create"`
+	ChangeDeadline     bool `json:"changeDeadline"`
+	ChecklistAddItems  bool `json:"checklistAddItems"`
+	AddFavorite        bool `json:"addFavorite"`
+	DeleteFavorite     bool `json:"deleteFavorite"`
+	Rate               bool `json:"rate"`
+	Take               bool `json:"take"`
+	EditOriginator     bool `json:"edit.originator"`
+	ChecklistReorder   bool `json:"checklist.reorder"`
+	ElapsedTimeAdd     bool `json:"elapsedtime.add"`
+	DayplanTimerToggle bool `json:"dayplan.timer.toggle"`
+	EditPlan           bool `json:"edit.plan"`
+	ChecklistAdd       bool `json:"checklist.add"`
+	FavoriteAdd        bool `json:"favorite.add"`
+	FavoriteDelete     bool `json:"favorite.delete"`
+}
+
+type CheckListTree struct {
+	NodeId      int                 `json:"nodeId"`
+	Fields      CheckListTreeFields `json:"fields"`
+	Action      []BitrixAction      `json:"action"`
+	Descendants []CheckListTree     `json:"descendants"`
+}
+
+type CheckListTreeFields struct {
+	Id               string                  `json:"id"`
+	CopiedId         *string                 `json:"copiedId"`
+	EntityId         *string                 `json:"entityId"`
+	UserId           *int                    `json:"userId"`
+	CreatedBy        *string                 `json:"createdBy"`
+	ParentId         *string                 `json:"parentId"`
+	Title            string                  `json:"title"`
+	SortIndex        *string                 `json:"sortIndex"`
+	DisplaySortIndex *string                 `json:"displaySortIndex"`
+	IsComplete       bool                    `json:"isComplete"`
+	IsImportant      bool                    `json:"isImportant"`
+	CompletedCount   int                     `json:"completedCount"`
+	Members          []BitrixUserDescription `json:"members"`
+	Attachments      []any                   `json:"attachments"`
+	NodeId           *string                 `json:"nodeId"`
+}
--- tasks.go
+++ tasks.go
@@ -7,7 +7,81 @@ import (
 )
 
 type Task struct {
-	CustomFields map[string]any `json:"customFields"`
+	Id                   string                  `json:"id"`
+	ParentId             string                  `json:"parentId"`
+	Title                string                  `json:"title"`
+	Description          string                  `json:"description"`
+	ChatId               *int                    `json:"chatId"`
+	Mark                 string                  `json:"mark"`
+	Priority             string                  `json:"priority"`
+	Multitask            BitrixBoolean           `json:"multitask"`
+	NotViewed            BitrixBoolean           `json:"notViewed"`
+	Replicate            BitrixBoolean           `json:"replicate"`
+	StageId              *string                 `json:"stageId"`
+	SprintId             *string                 `json:"sprintId"`
+	BacklogId            *string                 `json:"backlogId"`
+	CreatedBy            *string                 `json:"createdBy"`
+	CreatedDate          *string                 `json:"createdDate"`
+	ResponsibleId        *string                 `json:"responsibleId"`
+	ChangedBy            *string                 `json:"changedBy"`
+	ChangedDate          *string                 `json:"changedDate"`
+	StatusChangedBy      *string                 `json:"statusChangedBy"`
+	ClosedBy             *string                 `json:"closedBy"`
+	ClosedDate           *string                 `json:"closedDate"`
+	ActivityDate         *string                 `json:"activityDate"`
+	DateStart            *string                 `json:"dateStart"`
+	Deadline             *string                 `json:"deadline"`
+	StartDatePlan        *string                 `json:"startDatePlan"`
+	EndDatePlan          *string                 `json:"endDatePlan"`
+	GUID                 *string                 `json:"guid"`
+	XMLID                *string                 `json:"xmlId"`
+	CommentsCount        *string                 `json:"commentsCount"`
+	ServiceCommentsCount *string                 `json:"serviceCommentsCount"`
+	AllowChangeDeadline  BitrixBoolean           `json:"allowChangeDeadline"`
+	AllowTimeTracking    BitrixBoolean           `json:"allowTimeTracking"`
+	TaskControl          BitrixBoolean           `json:"taskControl"`
+	AddInReport          BitrixBoolean           `json:"addInReport"`
+	ForkedByTemplateId   *string                 `json:"forkedByTemplateId"`
+	TimeEstimate         *string                 `json:"timeEstimate"`
+	TimeSpentInLogs      *string                 `json:"timeSpentInLogs"`
+	MatchWorkTime        BitrixBoolean           `json:"matchWorkTime"`
+	ForumTopicId         *string                 `json:"forumTopicId"`
+	ForumId              *string                 `json:"forumId"`
+	SiteId               *string                 `json:"siteId"`
+	Subordinate          BitrixBoolean           `json:"subordinate"`
+	ExchangeModified     *string                 `json:"exchangeModified"`
+	ExchangeId           *string                 `json:"exchangeId"`
+	OutlookVersion       *string                 `json:"outlookVersion"`
+	ViewedDate           *string                 `json:"viewedDate"`
+	Sorting              *string                 `json:"sorting"`
+	DurationFact         *string                 `json:"durationFact"`
+	IsMuted              BitrixBoolean           `json:"isMuted"`
+	IsPinned             BitrixBoolean           `json:"isPinned"`
+	IsPinnedInGroup      BitrixBoolean           `json:"isPinnedInGroup"`
+	FlowId               *string                 `json:"flowId"`
+	DescriptionInBbcode  BitrixBoolean           `json:"descriptionInBbcode"`
+	Status               string                  `json:"status"`
+	StatusChangedDate    *string                 `json:"statusChangedDate"`
+	DurationPlan         *string                 `json:"durationPlan"`
+	DurationType         *string                 `json:"durationType"`
+	Favorite             BitrixBoolean           `json:"favorite"`
+	GroupId              *string                 `json:"groupId"`
+	Auditors             []string                `json:"auditors"`
+	Accomplices          []string                `json:"accomplices"`
+	Checklist            []BitrixChecklistItem   `json:"checklist"`
+	Group                BitrixGroup             `json:"group"`
+	Creator              BitrixUserDescription   `json:"creator"`
+	Responsible          BitrixUserDescription   `json:"responsible"`
+	AccomplicesData      []BitrixUserDescription `json:"accomplicesData"`
+	AuditorsData         any                     `json:"auditorsData"`
+	NewCommentsCount     int                     `json:"newCommentsCount"`
+	Action               BitrixAction            `json:"action"`
+	CheckListTree        CheckListTree           `json:"checkListTree"`
+	CheckListCanAdd      BitrixBoolean           `json:"checkListCanAdd"`
+	UFCRMTask            any                     `json:"ufCrmTask"`
+	UFTaskWebDavFiles    any                     `json:"ufTaskWebdavFiles"`
+	UfMailMessage        *string                 `json:"ufMailMessage"`
+	CustomFields         map[string]any          `json:"customFields"`
 }
 
 type TasksResponse struct {
@@ -16,7 +90,9 @@ type TasksResponse struct {
 
 const taskListMethod = "tasks.task.list"
 
-var taskKnownFields = map[string]struct{}{}
+var taskKnownFields = map[string]struct{}{
+	"id": {},
+}
 
 func (t *Task) UnmarshalJSON(data []byte) error {
 	type Alias Task
